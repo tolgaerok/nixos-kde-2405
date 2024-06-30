2b009c51a60e6cd62bbaeb0bbd671a0b6ff7c967
Tolga Erok <kingtolga@gmail.com>
2024-06-30 20:57:46 +0800

    (ツ)_/¯ Edit: 30-06-2024 08:57:46 PM

--- core/modules/custom-pkgs/git_upload.nix
+++ core/modules/custom-pkgs/git_upload.nix
@@ -12,37 +12,37 @@ let
 
     set -e
 
-    # Directory of your Git repository  *** --
+    # Directory of your Git repository
     REPO_DIR="/etc/nixos"
 
     # Commit message with timestamp and custom changes
     COMMIT_MSG="(ツ)_/¯ Edit: $(date '+%d-%m-%Y %I:%M:%S %p')"
 
-    # Ensure the Git repository is initialized and 'origin' remote is set--
+    # Ensure the Git repository is initialized and 'origin' remote is set
     if [ ! -d "$REPO_DIR/.git" ]; then
         echo "Initializing Git repository in $REPO_DIR..."
-        sudo git init "$REPO_DIR"
-        sudo git -C "$REPO_DIR" remote add origin git@github.com:tolgaerok/nixos-kde-2405.git
+        git init "$REPO_DIR"
+        git -C "$REPO_DIR" remote add origin git@github.com:tolgaerok/nixos-kde-2405.git
     fi
 
-    # Set user identity for Git
-    sudo git -C "$REPO_DIR" config user.name "Your Name"
-    sudo git -C "$REPO_DIR" config user.email "you@example.com"
+    # Set user identity for Git (adjust these lines accordingly)
+    git -C "$REPO_DIR" config user.name "Tolga Erok"
+    git -C "$REPO_DIR" config user.email "kingtolga@gmail.com"
 
     # Navigate to the repository directory
     cd "$REPO_DIR" || exit
 
     # Add all changes
-    sudo git add .
+    git add .
 
     # Commit changes with custom message
-    sudo git commit -am "$COMMIT_MSG"
+    git commit -am "$COMMIT_MSG"
 
     # Pull changes from remote repository to avoid conflicts
-    sudo git pull --rebase origin master
+    git pull --rebase origin main
 
     # Push changes to remote repository
-    sudo git push origin master
+    git push origin main
 
     # Notification of completion
     echo "Files uploaded successfully."
