521ee95864d60a20f1af6cb803f39bfcbce367b7
Tolga Erok <kingtolga@gmail.com>
2024-07-06 19:13:32 +0800

    (ツ)_/¯ Edit: 06-07-2024 07:13:32 PM

--- core/boot/mounts/default.nix
+++ core/boot/mounts/default.nix
@@ -19,7 +19,7 @@ in
     fsType = "cifs";
     options =
       let
-        # this line prevents hanging on network split
+        # Network mount Options
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
       [ "${automount_opts},credentials=/etc/nixos/network/smb-secrets,uid=1000,gid=100" ];
